0ef909633e2076d78bb0a6abc26f4398c48c98fd
cuongyura <c95.ficf@gmail.com>
2020-10-19 11:17:15 +0700

    upadte

---
+++ _1_relational_database/bai_tap/1_excute_sql_statements/executesqlstatements.sql
@@ -0,0 +1,9 @@
+-- SELECT * FROM classicmodels.customers
+SELECT * FROM customer;
+SELECT customerName, phone, city, country FROM customer;
+SELECT * FROM customers WHERE customerName = 'Atelier Graphique';
+SELECT * FROM customers WHERE customerName = 'Atelier Graphique';
+SELECT * FROM customers WHERE customername like '%A%';
+SELECT * FROM customers WHERE customername like '%A%';
+SELECT * FROM customers WHERE city IN ('Nantes',' Las Vegas',' Warszawa','NYC');
+SELECT * FROM customers WHERE city IN ('Nantes',' Las Vegas',' Warszawa','NYC')

5eee871dd0281240ad7f30e4ce3c10f948aec0af
cuongyura <c95.ficf@gmail.com>
2020-11-03 16:32:24 +0700

    update

--- _9_website_application_overview/thuc_hanh/productdiscountcalculator/src/CaculatorServlet.java
+++ _9_website_application_overview/thuc_hanh/productdiscountcalculator/src/CaculatorServlet.java
@@ -4,11 +4,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "CaculatorServlet", urlPatterns = "/caculator")
+@WebServlet(name = "CaculatorServlet",urlPatterns ="/caculator" )
 public class CaculatorServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String productDescription = request.getParameter("productDescription");
+        float listPrice = Float.parseFloat(request.getParameter("listPrice"));
+        float discountPercent = Float.parseFloat(request.getParameter("discountPercent"));
+        float discountAmount = listPrice * discountPercent * 0.01f;
 
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1> Product Description: " + productDescription + "</h1>");
+        writer.println("<h1> listPrice: " + listPrice + "</h1>");
+        writer.println("<h1> Discount Percent: " + discountPercent + "</h1>");
+        writer.println("<h1> Discount Amount: " + discountAmount + "</h1>");
+        writer.println("</html>");
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
